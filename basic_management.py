4e3415cbfe3643d896bfda029ed43b159265eb38
--global <--global>
2022-12-15 09:56:23 -0600

    Use d_w.py

--- basic_management.py
+++ basic_management.py
@@ -1,6 +1,6 @@
-from subprocess import getoutput
-import docker_parser
+import docker_wrapper
 import pprint
 
 # Goals: Loop: Select container (or exit), then menu to do stuff to container (start, stop, rm, list info, go back to main menu)
-pprint.pprint(docker_parser.DockerParser.ps())
+pprint.pprint(docker_wrapper.Docker.ps())
+pprint.pprint(docker_wrapper.Docker.containers_info())
